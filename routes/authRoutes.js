81f8d7b1164dcaa04e3f22cf7e9d8ac4d46188e8
pulakhadamumamaitreyi <160453397+pulakhadamumamaitreyi@users.noreply.github.com>
2026-02-15 13:21:26 +0530

    Implement user registration and login routes
    Added user registration and login routes with JWT authentication.

---
+++ routes/authRoutes.js
@@ -0,0 +1,45 @@
+import express from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+const router = express.Router();
+
+// Register
+router.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const existing = await User.findOne({ email });
+  if (existing) return res.status(400).json({ message: "User exists" });
+
+  const hashed = await bcrypt.hash(password, 10);
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashed
+  });
+
+  res.json({ message: "User Registered" });
+});
+
+// Login
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) return res.status(400).json({ message: "Invalid credentials" });
+
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) return res.status(400).json({ message: "Invalid credentials" });
+
+  const token = jwt.sign(
+    { id: user._id },
+    process.env.JWT_SECRET,
+    { expiresIn: "1d" }
+  );
+
+  res.json({ token });
+});
+
+export default router;
